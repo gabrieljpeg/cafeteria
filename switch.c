37ff9278fb62f7159a109f687067993392cde716
Gabriel <127786524+gabrieljpeg@users.noreply.github.com>
2024-08-22 20:04:54 -0300

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    //Apresenta o menu
+    printf("Menu\n");
+    printf("1 - Espresso\n");
+    printf("2 - Cappuccino\n");
+    printf("3 - Macchiato\n");
+    printf("Escolha uma opção: ");
+
+    //Lê a opção escolhida
+    int opcao;
+    scanf("%d", &opcao);
+
+    //Seleção da opção
+    switch(opcao){
+        case 1:
+            printf("Espresso escolhido.");
+            break;
+        case 2:
+            printf("Cappuccino escolhido.");
+            break;
+        case 3:
+            printf("Macchiato escolhido.");
+            break;
+    }
+
+    return 0;
+}
